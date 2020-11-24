bab8013f027fcad5fa4689b559bcc27b2b702d62
delankallen <delankallen@gmail.com>
2020-11-24 15:55:40 -0700

    removed commented code

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -1,6 +1,5 @@
 open SuaveRestApi
 open Suave.Web
-open Suave.Successful
 
 [<EntryPoint>]
 let main argv =
@@ -11,18 +10,4 @@ let main argv =
 
     startWebServer defaultConfig personWebPart
 
-    0 |> ignore
-
-    // let app =
-    //     choose
-    //         [GET >=> choose
-    //             [ path "/math/add" >=> browse ]
-    //         ]
-
-    // let app = 
-    //     choose [GET >=> choose [ path "/hello" >=> OK "Hello GET"; path "/goodbye" >=> OK "Good bye GET" ];
-    //             POST >=> choose [ path "/hello" >=> OK "Hello POST"; path "/goodbye" >=> OK "Good bye POST" ] ]
- 
-    // startWebServer defaultConfig (mapJson (fun (calc:Calc) -> { result = calc.a + calc.b }))
-    
-    0 // return an integer exit code
+    0
